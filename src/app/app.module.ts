6b0a41fc527312970eea487ac84ddd3900a5e0e7
Derek Wilson <u4hcsdaw@localhost>
2022-11-17 17:41:59 -0500

    @derekawilson72 made changes to filter the query results based on assignee pattern.  This filter uses the `filterIssues` method in the service module and filters based on input fields in the `issue-list.html`.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { IssueListComponent } from './components/issue-list/issue-list.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
